715881b9e2e81d44211dcc129451c3bf90ec0df9
Yudu <banyudu@gmail.com>
2023-08-31 10:39:24 +0800

    feat: support reading requirement id from git log

--- src/kemit.ts
+++ src/kemit.ts
@@ -2,16 +2,26 @@
 
 import branch from 'git-branch'
 import { spawn } from 'child_process'
+import gitLog from 'git-log'
 
-const commitMsgRegex = /^\[(需求|缺陷)\]\[\d+\]/
+const commitMsgRegex = /^\[(需求|缺陷)\]\[(\d+)\]/
 
-const getRequirementID = () => {
+const getRequirementID = async () => {
   try {
     const branchName = branch.sync();
     // 如果符合 feat/123/xxx 的格式，就返回 123
     const match = branchName.match(/^feat\/(\d+)\/.+/);
     if (match) {
       return match[1] ?? null;
+    }
+
+    // 如果不符合 feat/123/xxx 的格式，则尝试回溯 git log 中历史 commit 的 message
+    const logs = await gitLog({})
+    for (const log of logs) {
+      const match = log.subject.match(commitMsgRegex);
+      if (match) {
+        return match[2] ?? null;
+      }
     }
   } catch (error) {
     return null
@@ -21,8 +31,8 @@ const getRequirementID = () => {
 /**
  * 修改 argv 中的 commit message
  */
-const parseArgv = () => {
-  const requirementId = getRequirementID();
+const parseArgv = async () => {
+  const requirementId = await getRequirementID();
   if (!requirementId) {
     return
   }
@@ -36,6 +46,10 @@ const parseArgv = () => {
   }
 }
 
-parseArgv()
+const main = async () => {
+  await parseArgv()
 
-spawn('git', ['commit', ...process.argv.slice(2)], { stdio: 'inherit' })
+  spawn('git', ['commit', ...process.argv.slice(2)], { stdio: 'inherit' })
+}
+
+main().catch(console.error)
